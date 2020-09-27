452bbc9352f2d82385d89fcda68db106fefd1da8
IvanLunev <lun-vano@yandex.ru>
2020-09-27 15:59:13 +0300

    Boats added

--- lab3/sailboats.py
+++ lab3/sailboats.py
@@ -18,7 +18,7 @@ COLORS = {
     "BLACK": (0, 0, 0),
     "BROWN": (204, 102, 0),
     "PINK": (255, 153, 153),
-    "GRAY": (160, 160, 160),
+    "GRAY": (190, 190, 190),
 }
 
 
@@ -45,27 +45,63 @@ def draw_sun():
 
 
 def draw_cloud(coords, radius):
-
     circle(screen, COLORS["GRAY"], coords, radius, 2)
-    circle(screen, COLORS["WHITE"], coords, radius-2, 0)
+    circle(screen, COLORS["WHITE"], coords, radius - 2, 0)
 
-    circle(screen, COLORS["GRAY"], (coords[0] + radius//5*4, coords[1] - radius//5*4), radius, 2)
-    circle(screen, COLORS["WHITE"], (coords[0] + radius//5*4, coords[1] - radius//5*4), radius-2, 0)
+    circle(screen, COLORS["GRAY"], (coords[0] + radius // 5 * 4, coords[1] - radius // 5 * 4), radius, 2)
+    circle(screen, COLORS["WHITE"], (coords[0] + radius // 5 * 4, coords[1] - radius // 5 * 4), radius - 2, 0)
 
-    circle(screen, COLORS["GRAY"], (coords[0] + 2*radius//5*4, coords[1]), radius, 2)
-    circle(screen, COLORS["WHITE"], (coords[0] + 2*radius//5*4, coords[1]), radius-2, 0)
+    circle(screen, COLORS["GRAY"], (coords[0] + 2 * radius // 5 * 4, coords[1]), radius, 2)
+    circle(screen, COLORS["WHITE"], (coords[0] + 2 * radius // 5 * 4, coords[1]), radius - 2, 0)
 
-    circle(screen, COLORS["GRAY"], (coords[0] + 3*radius//5*4, coords[1] - radius//5*4), radius, 2)
-    circle(screen, COLORS["WHITE"], (coords[0] + 3*radius//5*4, coords[1] - radius//5*4), radius-2, 0)
+    circle(screen, COLORS["GRAY"], (coords[0] + 3 * radius // 5 * 4, coords[1] - radius // 5 * 4), radius, 2)
+    circle(screen, COLORS["WHITE"], (coords[0] + 3 * radius // 5 * 4, coords[1] - radius // 5 * 4), radius - 2, 0)
 
-    circle(screen, COLORS["GRAY"], (coords[0] + 4*radius//5*4, coords[1]), radius, 2)
-    circle(screen, COLORS["WHITE"], (coords[0] + 4*radius//5*4, coords[1]), radius-2, 0)
+    circle(screen, COLORS["GRAY"], (coords[0] + 4 * radius // 5 * 4, coords[1]), radius, 2)
+    circle(screen, COLORS["WHITE"], (coords[0] + 4 * radius // 5 * 4, coords[1]), radius - 2, 0)
 
 
 def draw_boat(coords, size):
-
-    rect(screen, COLORS["BROWN"], (*coords, size, size // 4))
+    rect(screen, COLORS["BROWN"], (coords[0], coords[1], size, size // 4))
     circle(screen, COLORS["BROWN"], coords, size // 4, 0, draw_bottom_left=True)
+    polygon(screen, COLORS["BROWN"], [
+        (coords[0] + size, coords[1]),
+        (coords[0] + 6 * size // 4, coords[1]),
+        (coords[0] + size, coords[1] + size // 4),
+        (coords[0] + size, coords[1]),
+    ])
+    circle(screen, COLORS["BLACK"], (coords[0] + 9*size//8, coords[1] + size//10), size // 15, 0)
+    circle(screen, COLORS["WHITE"], (coords[0] + 9*size//8, coords[1] + size//10), size // 25, 0)
+
+    line(screen, COLORS["BLACK"],
+         [coords[0] + size//3, coords[1]],
+         [coords[0] + size//3, coords[1] - size], size//20)
+
+    polygon(screen, COLORS["GRAY"], [
+        (coords[0] + size//40 + size//3, coords[1] - size),
+        (coords[0] + size//40 + size, coords[1] - size//2),
+        (coords[0] + size//40 + size//2, coords[1] - size//2),
+        (coords[0] + size//40 + size//3, coords[1] - size),
+    ])
+    polygon(screen, COLORS["GRAY"], [
+        (coords[0] + size//40 + size, coords[1] - size//2),
+        (coords[0] + size//40 + size//2, coords[1] - size//2),
+        (coords[0] + size//40 + size//3, coords[1]),
+        (coords[0] + size//40 + size, coords[1] - size//2),
+    ])
+
+    polygon(screen, COLORS["BLACK"], [
+        (coords[0] + size//40 + size//3, coords[1] - size),
+        (coords[0] + size//40 + size, coords[1] - size//2),
+        (coords[0] + size//40 + size//2, coords[1] - size//2),
+        (coords[0] + size//40 + size//3, coords[1] - size),
+    ], 1)
+    polygon(screen, COLORS["BLACK"], [
+        (coords[0] + size//40 + size, coords[1] - size//2),
+        (coords[0] + size//40 + size//2, coords[1] - size//2),
+        (coords[0] + size//40 + size//3, coords[1]),
+        (coords[0] + size//40 + size, coords[1] - size//2),
+    ], 1)
 
 
 draw_back()
@@ -75,7 +111,8 @@ draw_sun()
 draw_cloud((80, 130), 30)
 draw_cloud((300, 50), 20)
 
-draw_boat((200, 200), 100)
+draw_boat((100, 200), 100)
+draw_boat((300, 220), 150)
 
 pygame.display.update()
 clock = pygame.time.Clock()
